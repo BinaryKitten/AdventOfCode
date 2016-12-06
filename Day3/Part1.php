644869989ca3c912dfc9af9b01df59ec6448f757
Kathryn Reeve <kathryn@binarykitten.com>
2016-12-06 21:02:49 +0000

    update Day3 Part 3 - Generator'd code

--- Day3/Part1.php
+++ Day3/Part1.php
@@ -1,21 +1,32 @@
 <?php
 
-$data_file = fopen(__DIR__ . '/data.txt', 'rb');
+function check_triangle($side1, $side2, $side3)
+{
+    $result1 = ($side1 + $side2) > $side3;
+    $result2 = ($side2 + $side3) > $side1;
+    $result3 = ($side1 + $side3) > $side2;
 
-$invalid = 0;
-$valid   = 0;
-while ($line = fgets($data_file)) {
-    $items   = array_values(array_filter(explode(' ', $line)));
+    return ($result1 && $result2 && $result3);
+}
 
-    $result1 = ($items[0] + $items[1]) > $items[2];
-    $result2 = ($items[1] + $items[2]) > $items[0];
-    $result3 = ($items[0] + $items[2]) > $items[1];
-
-    if ($result1 && $result2 && $result3) {
-        $valid++;
-    } else {
-        $invalid++;
+function get_triangle($data_file)
+{
+    while ($line = fgets($data_file)) {
+        yield array_map(
+            'intval',
+            array_values(
+                array_filter(
+                    explode(' ', $line)
+                )
+            )
+        );
     }
+}
+
+$data_file = fopen(__DIR__ . '/data.txt', 'rb');
+$valid   = 0;
+foreach (get_triangle($data_file) as $triangle) {
+    $valid += check_triangle(...$triangle) ? 1 : 0;
 }
 echo 'There are ' . $valid . ' Triangles' . "\n";
 
