b4e23c2be24e5445be40cf11a8cd0b71b156698b
BinaryKitten <67553+BinaryKitten@users.noreply.github.com>
2023-12-07 10:40:59 +0000

    2023 Day 1 - Part1

---
+++ 2023/Day1/Part1.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * --- Day 1: Trebuchet?! ---
+ *
+ * Something is wrong with global snow production, and you've been selected to take a look.
+ * The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are
+ * likely to be having problems.
+ *
+ * You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars
+ * by December 25th.
+ *
+ * Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar;
+ * the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+ *
+ * You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending
+ * you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just
+ * say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading
+ * you into a trebuchet ("please hold still, we need to strap you in").
+ *
+ * As they're making the final adjustments, they discover that their calibration document (your puzzle input) has
+ * been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently,
+ * the Elves are having trouble reading the values on the document.
+ *
+ * The newly-improved calibration document consists of lines of text; each line originally contained a specific
+ * calibration value that the Elves now need to recover. On each line, the calibration value can be found by
+ * combining the first digit and the last digit (in that order) to form a single two-digit number.
+ *
+ * For example:
+ *
+ * 1abc2
+ * pqr3stu8vwx
+ * a1b2c3d4e5f
+ * treb7uchet
+ *
+ * In this example, the calibration values of these four lines are 12, 38, 15, and 77.
+ * Adding these together produces 142.
+ *
+ * Consider your entire calibration document. What is the sum of all of the calibration values?
+ */
+
+function calculationCalibrationSum(string $fileName): int
+{
+
+    $lines = array_map(trim(...), file(__DIR__.'/'.$fileName));
+    $numbers = [];
+    foreach ($lines as $line) {
+        $lineNumbers = [];
+        for ($i = 0, $l = strlen($line); $i < $l; $i++) {
+            $char = $line[$i];
+            if (is_numeric($char)) {
+                $lineNumbers[] = (int) $char;
+            }
+        }
+        if (count($lineNumbers) < 2) {
+            $lineNumbers[] = $lineNumbers[0];
+        }
+        $numbers[] = $lineNumbers[0].$lineNumbers[array_key_last($lineNumbers)];
+    }
+    return array_sum($numbers);
+}
+
+echo 'Example Result: ' . calculationCalibrationSum('example.txt') . "\n";
+echo 'Result: ' . calculationCalibrationSum('input.txt') . "\n\n";
+
+// Your puzzle answer was 54597.
